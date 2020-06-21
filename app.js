96a6ebd166f2b6eb124940903beede1c3f42fe06
Diego <diegosouzafl94@gmail.com>
2020-06-21 14:59:16 -0300

    adicionando router index.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express');
 const { query } = require('express');
 const app = express();
 
-//const indexRoute = require('./Routes/index');
+const indexRoute = require('./Routes/index');
 const userRoute = require('./Routes/users');
 
-//app.use('/', indexRoute);
+app.use('/', indexRoute);
 app.use('/users', userRoute);
 
 app.listen(3000);
