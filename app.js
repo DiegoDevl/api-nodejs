6281637d2b1c3cbb6bc289daa9b561539f3be966
Diego <diegosouzafl94@gmail.com>
2020-06-27 21:35:45 -0300

    conectando com o mongoDB Atlas

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const { query } = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const url = 'mongodb+srv://usuario_admm:152001@clusterapi1.unejk.mongodb.net/test?retryWrites=true&w=majority'
+const options = { poolSize: 5, useNewUrlParser: true, useUnifiedTopology: true };
+mongoose.connect(url, options);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connection.on('error', (err) => {
+    console.log('Erro na conexão com o banco de dados:'+ err);
+});
+mongoose.connection.on('disconnected', () => {
+    console.log('Aplicação desconectada do banco de dados!');
+});
+mongoose.connection.on('connected', () => {
+    console.log('Aplicação conectada do banco de dados!');
+});
+
+
+//BODY PARSER
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 const indexRoute = require('./Routes/index');
 const userRoute = require('./Routes/users');
