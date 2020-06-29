477e8f9a6fcbe06885c087907f67bd0c188900e3
Diego <diegosouzafl94@gmail.com>
2020-06-28 23:04:00 -0300

    alterando conexão mongoDB

--- app.js
+++ app.js
@@ -4,19 +4,22 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const url = 'mongodb+srv://usuario_admm:152001@clusterapi1.unejk.mongodb.net/test?retryWrites=true&w=majority'
+const url = 'mongodb+srv://usuario_adm:152001@clusterapi1.unejk.mongodb.net/test?retryWrites=true&w=majority'
 const options = { poolSize: 5, useNewUrlParser: true, useUnifiedTopology: true };
+
 mongoose.connect(url, options);
 mongoose.set('useCreateIndex', true);
 
 mongoose.connection.on('error', (err) => {
     console.log('Erro na conexão com o banco de dados:'+ err);
 });
+
 mongoose.connection.on('disconnected', () => {
     console.log('Aplicação desconectada do banco de dados!');
 });
+
 mongoose.connection.on('connected', () => {
-    console.log('Aplicação conectada do banco de dados!');
+    console.log('Aplicação conectada no banco de dados!');
 });
 
 
